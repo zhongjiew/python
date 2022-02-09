8701f345885bc4948e95e2fe01176a26b5f0cfe6
zhongjiew <wangzhongjie@stu.xmu.edu.cn>
2022-02-09 10:38:23 +0100

    Add files via upload
    it will generate two files for paired reads of Forward and Reverse files, separately, and one file for orphan reads of both Forward and Reverse files combined. So, three output files will be generated. The first two files contain the reads of the pairs that match, and the third contains orphan reads for both f and r reads combined.
    
    Usage: 
    python fq_splitting_pairedreads.py forward.fq reverse.fq

---
+++ fq_splitting_pairedreads.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+
+"""
+# author: Zhongjie Wang
+# date: 2022-02-07
+Description: extract paired reads and orphan reads from Forward and Reverse fastq files, respectively.
+
+Usage: 
+python fq_splitting_pairedreads.py forward.fq reverse.fq  
+
+forward.fq = LEFT  fastq or fastq.gz file (R1)
+reverse.fq = RIGHT fastq or fastq.gz file (R2)
+
+Three output files are generated. The first two files contain the reads of the
+    pairs that match and the third contains the solitary reads.
+
+"""
+
+# Importing modules
+import gzip
+import sys
+
+# Parsing user input
+try:
+    in1 = sys.argv[1]
+    in2 = sys.argv[2]
+except:
+    print(__doc__)
+    sys.exit(1)
+
+try:
+    separator = sys.argv[3]
+    if separator == "None":
+        separator = None
+except:
+    separator = " "
+
+# Defining classes
+class Fastq(object):
+    """Fastq object with name and sequence
+    """
+
+    def __init__(self, name, seq, name2, qual):
+        self.name = name
+        self.seq = seq
+        self.name2 = name2
+        self.qual = qual
+
+    def getShortname(self, separator):
+        if separator:
+            return separator.join(self.name.split(separator)[:-1])
+        else:
+            return self.name
+
+    def write_to_file(self, handle):
+        handle.write(self.name + "\n")
+        handle.write(self.seq + "\n")
+        handle.write(self.name2 + "\n")
+        handle.write(self.qual + "\n")
+
+# Defining functions
+def myopen(infile, mode="rt"):
+    if infile.endswith(".gz"):
+        return gzip.open(infile, mode=mode)
+    else:
+        return open(infile, mode=mode)
+
+def fastq_parser(infile):
+    """Takes a fastq file infile and returns a fastq object iterator
+    """
+    
+    with myopen(infile) as f:
+        while True:
+            name = f.readline().strip()
+            if not name:
+                break
+
+            seq = f.readline().strip()
+            name2 = f.readline().strip()
+            qual = f.readline().strip()
+            yield Fastq(name, seq, name2, qual)
+
+# Main
+if __name__ == "__main__":
+    seq1_dict = {}
+    seq2_dict = {}
+    seq1 = fastq_parser(in1)
+    seq2 = fastq_parser(in2)
+    s1_finished = False
+    s2_finished = False
+
+    if in1.endswith('.gz'): 
+    	outSuffix='.fastq.gz'
+    else:
+    	outSuffix='.fastq'
+    	
+    with myopen(in1 + "_pairs_R1" + outSuffix, "wt") as out1:
+        with myopen(in2 + "_pairs_R2" + outSuffix, "wt") as out2:
+            with myopen(in1 + "_singles" + outSuffix, "wt") as out3:
+                while not (s1_finished and s2_finished):
+                    try:
+                        s1 = next(seq1)
+                    except:
+                        s1_finished = True
+
+                    try:
+                        s2 = next(seq2)
+                    except:
+                        s2_finished = True
+
+                    # Add new sequences to hashes
+                    if not s1_finished:
+                        seq1_dict[s1.getShortname(separator)] = s1
+
+                    if not s2_finished:
+                        seq2_dict[s2.getShortname(separator)] = s2
+
+                    if not s1_finished and s1.getShortname(separator) in seq2_dict:
+                        seq1_dict[s1.getShortname(separator)].write_to_file(out1)
+                        seq1_dict.pop(s1.getShortname(separator))
+                        seq2_dict[s1.getShortname(separator)].write_to_file(out2)
+                        seq2_dict.pop(s1.getShortname(separator))
+
+                    if not s2_finished and s2.getShortname(separator) in seq1_dict:
+                        seq2_dict[s2.getShortname(separator)].write_to_file(out2)
+                        seq2_dict.pop(s2.getShortname(separator))
+                        seq1_dict[s2.getShortname(separator)].write_to_file(out1)
+                        seq1_dict.pop(s2.getShortname(separator))
+                        
+                # Treat all unpaired reads
+                for r in seq1_dict.values():
+                    r.write_to_file(out3)
+
+                for r in seq2_dict.values():
+                    r.write_to_file(out3)
+
